3db00924dd091a7146ecaffc95a6a267ccc466a5
Nellark <nelakulati@gmail.com>
2025-07-08 15:18:16 +0200

    added environments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', redirectTo: 'products', pathMatch: 'full' },
+    // { path: 'products', component: ProductListComponent },
+    // { path: 'products/add', component: ProductFormComponent },
+    // { path: 'products/edit/:id', component: ProductFormComponent },
+    // { path: 'suppliers', component: SupplierListComponent },
+    // { path: 'suppliers/add', component: SupplierFormComponent },
+    // { path: 'suppliers/edit/:id', component: SupplierFormComponent },
+  ];
+
+
---
+++ src/app/services/procuct.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environments';
+
+@Injectable({ providedIn: 'root' })
+export class ProductService {
+  private apiUrl = `${environment.apiUrl}/products`;
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
+  get(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
+  create(data: Product): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, data);
+  }
+
+  update(id: number, data: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, data);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
